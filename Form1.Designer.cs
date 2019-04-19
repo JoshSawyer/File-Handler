6e0c830e6f007c37702a3dc90463374377f97265
Josh <joshuasawyerphone@gmail.com>
2019-04-19 23:55:08 +0100

    Added First Functionality
    + Functions
    + Forms elements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.fileList = new System.Windows.Forms.TextBox();
+            this.dirInput = new System.Windows.Forms.TextBox();
+            this.buttonOption1 = new System.Windows.Forms.Button();
+            this.deleteFiles = new System.Windows.Forms.Button();
+            this.clearFiles = new System.Windows.Forms.Button();
+            this.optionOneTitle = new System.Windows.Forms.Label();
+            this.optionTwoTitle = new System.Windows.Forms.Label();
+            this.fileInput = new System.Windows.Forms.TextBox();
+            this.searchAllSubs = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // fileList
+            // 
+            this.fileList.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.fileList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.fileList.Location = new System.Drawing.Point(12, 12);
+            this.fileList.Multiline = true;
+            this.fileList.Name = "fileList";
+            this.fileList.ReadOnly = true;
+            this.fileList.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.fileList.Size = new System.Drawing.Size(388, 284);
+            this.fileList.TabIndex = 0;
+            this.fileList.WordWrap = false;
+            // 
+            // dirInput
+            // 
+            this.dirInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dirInput.Location = new System.Drawing.Point(406, 32);
+            this.dirInput.MaxLength = 200;
+            this.dirInput.Name = "dirInput";
+            this.dirInput.Size = new System.Drawing.Size(366, 20);
+            this.dirInput.TabIndex = 1;
+            this.dirInput.Tag = "";
+            // 
+            // buttonOption1
+            // 
+            this.buttonOption1.Location = new System.Drawing.Point(406, 134);
+            this.buttonOption1.Name = "buttonOption1";
+            this.buttonOption1.Size = new System.Drawing.Size(366, 23);
+            this.buttonOption1.TabIndex = 2;
+            this.buttonOption1.Text = "Generate List";
+            this.buttonOption1.UseVisualStyleBackColor = true;
+            this.buttonOption1.Click += new System.EventHandler(this.ButtonOption1_Click);
+            // 
+            // deleteFiles
+            // 
+            this.deleteFiles.Enabled = false;
+            this.deleteFiles.Location = new System.Drawing.Point(12, 302);
+            this.deleteFiles.Name = "deleteFiles";
+            this.deleteFiles.Size = new System.Drawing.Size(184, 47);
+            this.deleteFiles.TabIndex = 3;
+            this.deleteFiles.Text = "Delete";
+            this.deleteFiles.UseVisualStyleBackColor = true;
+            this.deleteFiles.Click += new System.EventHandler(this.DeleteFiles_Click);
+            // 
+            // clearFiles
+            // 
+            this.clearFiles.Enabled = false;
+            this.clearFiles.Location = new System.Drawing.Point(216, 302);
+            this.clearFiles.Name = "clearFiles";
+            this.clearFiles.Size = new System.Drawing.Size(184, 47);
+            this.clearFiles.TabIndex = 4;
+            this.clearFiles.Text = "Clear";
+            this.clearFiles.UseVisualStyleBackColor = true;
+            this.clearFiles.Click += new System.EventHandler(this.ClearFiles_Click);
+            // 
+            // optionOneTitle
+            // 
+            this.optionOneTitle.AutoSize = true;
+            this.optionOneTitle.Location = new System.Drawing.Point(406, 15);
+            this.optionOneTitle.Name = "optionOneTitle";
+            this.optionOneTitle.Size = new System.Drawing.Size(77, 13);
+            this.optionOneTitle.TabIndex = 5;
+            this.optionOneTitle.Text = "Enter Directory";
+            // 
+            // optionTwoTitle
+            // 
+            this.optionTwoTitle.AutoSize = true;
+            this.optionTwoTitle.Location = new System.Drawing.Point(406, 62);
+            this.optionTwoTitle.Name = "optionTwoTitle";
+            this.optionTwoTitle.Size = new System.Drawing.Size(229, 13);
+            this.optionTwoTitle.TabIndex = 7;
+            this.optionTwoTitle.Text = "Enter File or Extention (e.g. \"hello.txt\" or \"*.txt\")";
+            // 
+            // fileInput
+            // 
+            this.fileInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileInput.Location = new System.Drawing.Point(406, 79);
+            this.fileInput.MaxLength = 200;
+            this.fileInput.Name = "fileInput";
+            this.fileInput.Size = new System.Drawing.Size(366, 20);
+            this.fileInput.TabIndex = 6;
+            this.fileInput.Tag = "";
+            // 
+            // searchAllSubs
+            // 
+            this.searchAllSubs.AutoSize = true;
+            this.searchAllSubs.Checked = true;
+            this.searchAllSubs.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.searchAllSubs.Location = new System.Drawing.Point(406, 111);
+            this.searchAllSubs.Name = "searchAllSubs";
+            this.searchAllSubs.Size = new System.Drawing.Size(144, 17);
+            this.searchAllSubs.TabIndex = 10;
+            this.searchAllSubs.Text = "Search All Subdirectories";
+            this.searchAllSubs.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.searchAllSubs);
+            this.Controls.Add(this.optionTwoTitle);
+            this.Controls.Add(this.fileInput);
+            this.Controls.Add(this.optionOneTitle);
+            this.Controls.Add(this.clearFiles);
+            this.Controls.Add(this.deleteFiles);
+            this.Controls.Add(this.buttonOption1);
+            this.Controls.Add(this.dirInput);
+            this.Controls.Add(this.fileList);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -43,10 +156,21 @@
             this.Text = "File Handler";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox fileList;
+        private System.Windows.Forms.TextBox dirInput;
+        private System.Windows.Forms.Button buttonOption1;
+        private System.Windows.Forms.Button deleteFiles;
+        private System.Windows.Forms.Button clearFiles;
+        private System.Windows.Forms.Label optionOneTitle;
+        private System.Windows.Forms.Label optionTwoTitle;
+        private System.Windows.Forms.TextBox fileInput;
+        private System.Windows.Forms.CheckBox searchAllSubs;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace File_Handler
 {
     public partial class Main : Form
     {
+        bool debugMode = true;
+
+        string memoryDir;
+        bool? memoryOption;
+        string memoryFile;
         public Main()
         {
             InitializeComponent();
@@ -19,7 +25,129 @@ namespace File_Handler
 
         private void Main_Load(object sender, EventArgs e)
         {
-            this.BackColor = Color.Gray;
+            memoryDir = "";
+            memoryOption = null;
+            memoryFile = "";
+        }
+
+        private string[] GetFiles(string dir, string file, bool allDir)
+        {
+            memoryDir = dir;
+            memoryOption = allDir;
+            memoryFile = file;
+
+            try
+            {
+                if (allDir)
+                {
+                    return Directory.GetFiles(dir, file, SearchOption.AllDirectories);
+                }
+                else
+                {
+                    return Directory.GetFiles(dir, file);
+                }
+            }
+            catch (Exception error)
+            {
+                string errStr = error.ToString();
+                MessageBox.Show(errStr.Substring(errStr.IndexOf(":"), errStr.IndexOf("." + Environment.NewLine)), "Error");
+                string[] returnDummy = { "Error" };
+                return returnDummy;
+            }
+        }
+
+        private void ButtonOption1_Click(object sender, EventArgs e)
+        {
+            string dir = dirInput.Text;
+
+            string[] files;
+            try
+            {
+                if (!Directory.Exists(dir))
+                {
+                    MessageBox.Show("Directory does not exist!", "Directory Error");
+                    return;
+                }
+                if (fileList.Text != "")
+                {
+                    MessageBox.Show("There is already a file list!", "List Error");
+                    return;
+                }
+
+                files = GetFiles(dir, fileInput.Text, searchAllSubs.Checked);
+                if (files[0] == "Error")
+                {
+                    return;
+                }
+            }
+            catch (Exception error)
+            {
+                string errStr = error.ToString();
+                MessageBox.Show(errStr.Substring(errStr.IndexOf(":"), errStr.IndexOf("." + Environment.NewLine)), "Error");
+                return;
+            }
+
+            if (files.Length < 1)
+            {
+                MessageBox.Show("No files found!", "Directory Error");
+                return;
+            }
+
+            foreach (string fileDir in files)
+            {
+                fileList.Text += fileDir + Environment.NewLine;
+            }
+
+            deleteFiles.Enabled = true;
+            clearFiles.Enabled = true;
+        }
+
+        private void DeleteFiles_Click(object sender, EventArgs e)
+        {
+            DialogResult confirmDelete = MessageBox.Show("Are you sure you want to delete these files?",
+                "Confirm Deletion",
+                MessageBoxButtons.YesNo);
+
+            if (confirmDelete == DialogResult.No)
+            {
+                return;
+            }
+
+            try
+            {
+                string[] files = GetFiles(memoryDir, memoryFile, memoryOption ?? false);
+
+                if (!debugMode)
+                {
+                    MessageBox.Show("Deletion");
+                    MessageBox.Show("Files successfully deleted!", "Success");
+
+                    ClearFiles_Click(sender, e);
+                    return;
+                }
+
+                foreach (string fileDir in files)
+                {
+                    File.Delete(fileDir);
+                }
+            }
+            catch (Exception error)
+            {
+                string errStr = error.ToString();
+                MessageBox.Show(errStr.Substring(errStr.IndexOf(":"), errStr.IndexOf("." + Environment.NewLine)), "Error");
+                return;
+            }
+
+            MessageBox.Show("Files successfully deleted!", "Success");
+
+            ClearFiles_Click(sender, e);
+        }
+
+        private void ClearFiles_Click(object sender, EventArgs e)
+        {
+            fileList.Text = "";
+            deleteFiles.Enabled = false;
+            clearFiles.Enabled = false;
         }
     }
 }
